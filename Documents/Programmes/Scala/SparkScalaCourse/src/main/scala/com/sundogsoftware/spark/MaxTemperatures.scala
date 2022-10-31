aaaa3c2638fc4be88b747bc9ea42cf819da27102
Jamie Sadler <jamietsadler@hotmail.co.uk>
2022-10-31 15:34:01 +0000

    Min and Max Temperatures per Stations using map

--- Documents/Programmes/Scala/SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/MaxTemperatures.scala
+++ Documents/Programmes/Scala/SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/MaxTemperatures.scala
@@ -4,38 +4,51 @@ import org.apache.spark._
 import org.apache.log4j._
 import scala.math.max
 
-/** Find the maximum temperature by weather station for a year */
+/** Find the minimum temperature by weather station */
 object MaxTemperatures {
-  
-  def parseLine(line:String): (String, String, Float) = {
-    val fields = line.split(",")
-    val stationID = fields(0)
-    val entryType = fields(2)
-    val temperature = fields(3).toFloat * 0.1f * (9.0f / 5.0f) + 32.0f
-    (stationID, entryType, temperature)
+
+
+  def parseLine(line: String): (String, String, Float) = {
+    val fieldsToSplit = line.split( ",")
+    val station = fieldsToSplit(0)
+    val entry = fieldsToSplit(2)
+    val temp = fieldsToSplit(3).toFloat * 0.1f * (9.0f / 5.0f) * 32.0f
+    return (station, entry, temp)
+
   }
-    /** Our main function where the action happens */
+  /** Our main function where the action happens */
   def main(args: Array[String]) {
-   
+
     // Set the log level to only print errors
     Logger.getLogger("org").setLevel(Level.ERROR)
-    
+
     // Create a SparkContext using every core of the local machine
-    val sc = new SparkContext("local[*]", "MaxTemperatures")
-    
+    val sc = new SparkContext("local[*]", "MaxTemps")
+    // Read each line of input data
     val lines = sc.textFile("data/1800.csv")
+
+    // Convert to (stationID, entryType, temperature) tuples
     val parsedLines = lines.map(parseLine)
+
+    // Filter out all but TMIN entries
     val maxTemps = parsedLines.filter(x => x._2 == "TMAX")
+
+    // Convert to (stationID, temperature)
     val stationTemps = maxTemps.map(x => (x._1, x._3.toFloat))
-    val maxTempsByStation = stationTemps.reduceByKey( (x,y) => max(x,y))
-    val results = maxTempsByStation.collect()
-    
+
+    // Reduce by stationID retaining the max temperature found
+
+    val maxTempsPerStation = stationTemps.reduceByKey( (x, y) => max(x, y))
+
+    // Collect, format, and print the results
+    val results = maxTempsPerStation.collect()
+
     for (result <- results.sorted) {
-       val station = result._1
-       val temp = result._2
-       val formattedTemp = f"$temp%.2f F"
-       println(s"$station max temperature: $formattedTemp") 
+      val station = result._1
+      val temp = result._2
+      val formattedTemp = f"$temp%.2f F"
+      println(s"$station maximum temperature: $formattedTemp")
     }
-      
+
   }
 }
--- Documents/Programmes/Scala/SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/MinTemperatures.scala
+++ Documents/Programmes/Scala/SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/MinTemperatures.scala
@@ -6,13 +6,15 @@ import scala.math.min
 
 /** Find the minimum temperature by weather station */
 object MinTemperatures {
-  
-  def parseLine(line:String): (String, String, Float) = {
-    val fields = line.split(",")
-    val stationID = fields(0)
-    val entryType = fields(2)
-    val temperature = fields(3).toFloat * 0.1f * (9.0f / 5.0f) + 32.0f
-    (stationID, entryType, temperature)
+
+
+  def parseLine(line: String): (String, String, Float) = {
+    val fieldsToSplit = line.split( ",")
+    val station = fieldsToSplit(0)
+    val entry = fieldsToSplit(2)
+    val temp = fieldsToSplit(3).toFloat * 0.1f * (9.0f / 5.0f) * 32.0f
+    return (station, entry, temp)
+
   }
     /** Our main function where the action happens */
   def main(args: Array[String]) {
@@ -21,8 +23,7 @@ object MinTemperatures {
     Logger.getLogger("org").setLevel(Level.ERROR)
     
     // Create a SparkContext using every core of the local machine
-    val sc = new SparkContext("local[*]", "MinTemperatures")
-    
+    val sc = new SparkContext("local[*]", "MinTemps")
     // Read each line of input data
     val lines = sc.textFile("data/1800.csv")
     
@@ -36,10 +37,11 @@ object MinTemperatures {
     val stationTemps = minTemps.map(x => (x._1, x._3.toFloat))
     
     // Reduce by stationID retaining the minimum temperature found
-    val minTempsByStation = stationTemps.reduceByKey( (x,y) => min(x,y))
+
+    val minTempsPerStation = stationTemps.reduceByKey( (x, y) => min(x, y))
     
     // Collect, format, and print the results
-    val results = minTempsByStation.collect()
+    val results = minTempsPerStation.collect()
     
     for (result <- results.sorted) {
        val station = result._1
